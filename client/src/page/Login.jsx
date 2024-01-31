06bfd843b7449b44a6fc78dd3758f561eb9a8c55
Rakxit-Shetty <rakshitshetty136@gmail.com>
2024-01-31 22:59:43 +0530

    inited Login UI

--- client/src/page/Login.jsx
+++ client/src/page/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-
+import logo from "../assets/rakki.jpg";
 import { useLoginMutation } from "../redux/apis/UserApi";
+import "./Login.css";
 
 const Login = () => {
 
@@ -19,22 +20,65 @@ const onInputChange=(e)=>{
   
 }
 
-const hitLoginApi=()=>{
+const hitLoginApi=(e)=>{
+  e.preventDefault()
 login(formData)
 }
 
   return (
     <>
-      <div>Login</div>
-<div>
+      <div className="login-container">
+        <form className="login-form" onSubmit={(e)=>hitLoginApi(e)}>
+
+        <div className="login-logo">
+          <img src={logo} alt="logo" />
+          </div>
+          
+          <div className="login-input">
+          <label htmlFor="email">Email</label>
+          <div className="form__input">
+            <input
+              required
+              id="email"
+              name="email"
+              type="email"
+              value={email} onChange={onInputChange}
+            />
+          </div>
+        </div>
+
+        <div className="login-input">
+          <label htmlFor="password">Password</label>
+          <div className="form__input">
+            <input
+              required
+              id="password"
+              name="password"
+              type="password"
+              value={password} onChange={onInputChange}
+            />
+          </div>
+        </div>
+
+        <div className="flex-center">
+          <button className="btn-primary btn-black btn-login flex-center">
+            Log in
+          </button>
+        </div>
+        </form>
+      </div>
+
+
+      {/* <div>Login</div> */}
+{/* <div>
   <label>Email :</label>
-<input type="text" name="email" value={email} onChange={onInputChange}/>
+<input type="text" name="email" />
 </div>
 <div>
   <label>Password :</label>
   <input type="password" name="password" value={password} onChange={onInputChange}/>
-  </div>
-<button onClick={hitLoginApi} >login</button>
+  </div> */}
+{/* <button onClick={hitLoginApi} >login</button> */}
     </>
   )
 }
